8265dc661f46fe2ba48d237db58bb91e81416d74
Peter Wood <peter@cowboyaliens.com>
2016-09-06 20:16:53 +0200

    In log debug mode the consul query is now logged

--- src/main/java/discovery/consul/ConsulDiscoverer.java
+++ src/main/java/discovery/consul/ConsulDiscoverer.java
@@ -3,16 +3,17 @@ package discovery.consul;
 import discovery.Discoverer;
 import discovery.DiscoveryException;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 
 import java.net.URI;
-import java.net.URL;
 import java.util.List;
 
 /**
  * © 2016 org.bytewood
  */
+@Slf4j
 public class ConsulDiscoverer implements Discoverer {
 
     private DiscoveryClient discoveryClient;
@@ -24,11 +25,15 @@ public class ConsulDiscoverer implements Discoverer {
     @Override
     @SneakyThrows
     public URI locate(String microservice) {
+        URI uri;
         List<ServiceInstance> microservices = discoveryClient.getInstances(microservice);
         if ((microservices != null) && !microservices.isEmpty() ) {
-            return microservices.get(0).getUri();
+            uri =  microservices.get(0).getUri();
         } else {
             throw new DiscoveryException(microservice + " is Undiscoverable") ;
         }
+
+        log.debug(microservice + " found @ " + uri.toString());
+        return uri;
     }
 }
